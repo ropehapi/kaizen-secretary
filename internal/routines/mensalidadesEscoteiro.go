8e2ebaa3c64eeabe12eba27bf9498c7015a6a068
ropehapi <ropehapi@gmail.com>
2026-03-06 02:12:01 -0300

    feat: Updating messenger-api consume

--- internal/routines/mensalidadesEscoteiro.go
+++ internal/routines/mensalidadesEscoteiro.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 func RememberScoutMonthlyFees() {
@@ -14,21 +15,27 @@ func RememberScoutMonthlyFees() {
 		"Pedrinho": "5543936180709",
 	}
 
+	month := getMonthInPortuguese()
+
 	for name, phone := range taxpayers {
+		message := fmt.Sprintf("Olá, %s, passando para lembrar sobre Contribuição mensal do Grupo Escoteiro Guarani, referente ao mês de %s. Enviar comprovante no whatsApp *PIX GRUPO GUARANI*.\nObs: Essa é uma mensagem automática. Caso já tenha feito o pagamento, por favor desconsidere.", name, month)
+		
 		payload := map[string]interface{}{
 			"number":  phone,
-			"message": "Olá " + name + " paga eu.",
+			"message": message,
 		}
 
 		body, _ := json.Marshal(payload)
 
-		req, err := http.NewRequest("POST", os.Getenv("MESSAGING_OFFICER_HOST")+":"+os.Getenv("MESSAGING_OFFICER_PORT")+"/send-message", bytes.NewBuffer(body))
+		req, err := http.NewRequest("POST", os.Getenv("MESSAGING_OFFICER_HOST")+":"+os.Getenv("MESSAGING_OFFICER_PORT")+"/api/send-message", bytes.NewBuffer(body))
 		if err != nil {
 			fmt.Println("Erro:", err)
 			return
 		}
 
 		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("x-api-key", "corinthians-gigante")
+		req.Header.Set("x-Session-Id", "pedro")
 
 		client := &http.Client{}
 		resp, err := client.Do(req)
@@ -50,4 +57,26 @@ func RememberScoutMonthlyFees() {
 			fmt.Println(string(respBody))
 		}
 	}
+}
+
+func getMonthInPortuguese() string {
+	portugueseMonths := [...]string{
+		"Janeiro", 
+		"Fevereiro", 
+		"Março", 
+		"Abril", 
+		"Maio", 
+		"Junho",
+		"Julho", 
+		"Agosto", 
+		"Setembro", 
+		"Outubro", 
+		"Novembro", 
+		"Dezembro",
+	}
+
+	actualMonth := time.Now().Month()
+	monthNamePortuguese := portugueseMonths[actualMonth-1]
+
+	return monthNamePortuguese
 }
