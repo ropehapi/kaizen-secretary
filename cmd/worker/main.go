b6668db8f22939f654915851c521a601d7700fd2
ropehapi <ropehapi@gmail.com>
2026-03-07 04:17:44 -0300

    fix: Resolve panic when loading .env

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
+	"log"
+
 	"github.com/joho/godotenv"
 	"github.com/robfig/cron/v3"
 	// "github.com/ropehapi/kaizen-secretary/internal/routines"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic("Error loading .env file")
+	if err := godotenv.Load(); err != nil {
+		log.Println("Aviso: arquivo .env não encontrado, usando variáveis de ambiente do sistema")
 	}
 
-    c := cron.New(cron.WithSeconds()) // habilita campo de segundos
+	c := cron.New(cron.WithSeconds()) // habilita campo de segundos
 
-    // "0 0 0 5 * *" = 00:00:00 do dia 10 de cada mês
-    // _, err = c.AddFunc("0 0 0 10 * *", routines.RememberScoutMonthlyFees)
-    // if err != nil {
-    //     panic(err)
-    // }
+	// "0 0 0 5 * *" = 00:00:00 do dia 10 de cada mês
+	// _, err = c.AddFunc("0 0 0 10 * *", routines.RememberScoutMonthlyFees)
+	// if err != nil {
+	//     panic(err)
+	// }
 
-    c.Start()
+	c.Start()
 
-    // mantém a aplicação rodando
-    select {}
+	// mantém a aplicação rodando
+	select {}
 }
